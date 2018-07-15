94dc3e4af3ab44b31da4cec9692d4669807f5193
CaiClone <caiskove@gmail.com>
2018-07-15 18:02:30 +0200

    Get/Set options

--- drafter.py
+++ drafter.py
@@ -1,3 +1,22 @@
 import eel
+from pokemontcgsdk import Card
+import numpy as np
 eel.init('web')
-eel.start('main.html')
+
+team = []
+options = []
+NOPTIONS=3
+
+@eel.expose
+def selectOption(opt):
+    team.append(opt)
+    eel.updateTeam(team)
+    return getOptions()
+
+@eel.expose
+def getOptions():
+    options = Card.where(set='generations', supertype='pokemon')
+    return [card.image_url_hi_res for card in np.random.choice(options,NOPTIONS,replace=False)]
+    #return ["https://images.pokemontcg.io/g1/RC2.png","https://images.pokemontcg.io/g1/23.png","https://images.pokemontcg.io/g1/45.png"] 
+
+eel.start('index.html', options ={'chromeFlags':["--start-fullscreen"]})
--- web/js/base.js
+++ web/js/base.js
@@ -1,10 +1,23 @@
-$(document).ready(()=>{
+
+function setOptions(cards){
+    $(".option > img").each(function() {
+        let newImage = cards[parseInt($(this).parent().attr('option'))];
+        $(this).attr('src',newImage);
+     });
+}
+$(document).ready(function(){
     let cnt=0;
     $(".option").each(function() {
        $(this).attr('option',cnt);
         cnt++;
     });
     $(".option").click(function(){
-        console.log($(this).attr('option'));
+        eel.selectOption($(this).attr('option'))(setOptions);
     });
+    
+    eel.expose(updateTeam);
+    function updateTeam(team){
+    }
+    eel.getOptions()(setOptions);
 });
+$("#deck").val(team.join("\n"));
