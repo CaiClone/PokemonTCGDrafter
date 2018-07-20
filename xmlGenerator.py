aa3c172c0f016817d6afe7467c215afe2c7c05f8
CaiClone <caiskove@gmail.com>
2018-07-21 01:59:39 +0200

    Xml generator

--- xmlGenerator.py
+++ xmlGenerator.py
@@ -1,4 +1,4 @@
-from pokemontcgsdk import Set
+from pokemontcgsdk import Set,Card
 from lxml import etree as et
 
 root = et.Element('cockatrice_carddatabase')
@@ -19,6 +19,34 @@ for s in Set.all():
     setType.text = s.series
     releasedate.text = s.release_date
 
+names = []
+n=0
+for c in Card.all():
+    n+=1
+    ncard = et.SubElement(xmlcards,"card")
+
+    name = et.SubElement(ncard,"name")
+    cset= et.SubElement(ncard,"set")
+    color = et.SubElement(ncard,"color")
+    mana = et.SubElement(ncard,"manacost")
+    cmc = et.SubElement(ncard,"cmc")
+    ctype = et.SubElement(ncard,"type")
+    pt = et.SubElement(ncard,"pt")
+    tablerow = et.SubElement(ncard,"tablerow")
+    text = et.SubElement(ncard,"text")
+    font = et.SubElement(ncard,"font")
+    
+    name.text = c.name +" "+ c.id
+    cset.text = c.set_code
+    cset.set('picURL',c.image_url_hi_res)
+    if(c.types!=None):
+        color.text = " ".join(c.types)
+    ctype.text = c.supertype +" - "+c.subtype
+    if(c.hp!=None and c.hp!="None"):
+        pt.text="0/"+str(int(int(c.hp)/10))
+    tablerow.text = "0"
+
+print(n)
 
 
 tree = et.ElementTree(root)
