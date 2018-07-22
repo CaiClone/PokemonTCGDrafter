5dd784a29b2693adeeae52fc82e52a106c26144a
CaiClone <caiskove@gmail.com>
2018-07-22 19:08:53 +0200

    frontend SetSelection

--- web/js/base.js
+++ web/js/base.js
@@ -5,7 +5,16 @@ function setOptions(cards){
         $(this).attr('src',newImage);
      });
 }
+function fillSets(sets){
+    const sg = $("#SetGrid");
+    sg.append(
+            `<div class="card" value="FB">
+                <img class="card-img-top setimg" src="https://images.pokemontcg.io/sm6/logo.png">
+            </div>`
+        );
+}
 $(document).ready(function(){
+    fillSets();
     let cnt=0;
     $(".option").each(function() {
        $(this).attr('option',cnt);
@@ -19,5 +28,16 @@ $(document).ready(function(){
     function updateTeam(team){
         $("#deck").val(team.join("\n"));     
     }
-    eel.getOptions()(setOptions);
 });
+$(".setimg").click(function(){
+   $(this).toggleClass("bg-primary"); 
+});
+$("#btnGoDraft").click(function(){
+    let sets = $(".bg-primary").parent().map(function() {return $(this).attr('value');});
+    eel.getOptions()(function(){
+        setOptions();
+        $("#SetSelector").hide();
+        $("#CardSelector").show();
+    });
+    
+})
