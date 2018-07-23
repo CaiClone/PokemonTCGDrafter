e0776c8a466715c4f2fa482e827677d22f62fb25
CaiClone <caiskove@gmail.com>
2018-07-23 19:40:00 +0200

    add Probabilities

--- drafter.py
+++ drafter.py
@@ -3,11 +3,18 @@ from pokemontcgsdk import Card,Set
 import numpy as np
 eel.init('web')
 
+#probs pokemon,trainer,energy
+probs= [("Pokemon",0.35),("Trainer",0.5),("Energy",0.15)]
+#rarities = [("Common",0.4),("Uncommon",0.20),("Rare",0.15),("Rare Ultra",0.1)]
+
+
 team = []
 options = []
+chosenSets =[]
 NOPTIONS=3
-def getTeamNames():
-    return [str(c.count)+" "+c.name+" "+c.id for c in team]
+def getTeam():
+    teamNames,teamImages,count = zip(*[((str(c.count)+" "+c.name+" "+c.id),c.image_url_hi_res,c.count) for c in team])
+    return [teamNames,teamImages,sum(count)]
 
 @eel.expose
 def selectOption(opt):
@@ -18,18 +25,33 @@ def selectOption(opt):
     except ValueError:
         sel.count=1
         team.append(sel)
-    eel.updateTeam(getTeamNames())
+    eel.updateTeam(getTeam())
     return getOptions()
 
 @eel.expose
 def getOptions():
     global options
-    options = np.random.choice(Card.where(setCode='sm6', supertype='pokemon'),NOPTIONS,replace=False)
+    global chosenSets
+    global probs
+    recalc=0
+    while(recalc<20):
+        cset = np.random.choice(chosenSets,1)
+        ctype = np.random.choice([p[0] for p in probs],p=[p[1] for p in probs])
+        try:
+            options = np.random.choice(Card.where(set=cset[0], supertype=ctype),NOPTIONS,replace=False)
+            recalc+=100
+        except ValueError:
+            recalc+=1
     return [card.image_url_hi_res for card in options]
 
 @eel.expose
 def getSets():
-    sets= [(s.code,s.name,s.symbol_url) for s in Set.all()]
+    sets= [(s.name,s.symbol_url) for s in Set.all()]
     return sets
-
-eel.start('index.html')#, options ={'chromeFlags':["--start-fullscreen"]})
+@eel.expose
+def setSets(sets):
+    global chosenSets
+    chosenSets = sets
+    copt= getOptions()
+    return copt
+eel.start('index.html', options ={'chromeFlags':["--start-fullscreen"]})
--- web/js/base.js
+++ web/js/base.js
@@ -4,6 +4,7 @@ function setOptions(cards){
         let newImage = cards[parseInt($(this).parent().attr('option'))];
         $(this).attr('src',newImage);
      });
+     $(".option").show();
 }
 function fillSets(sets){
     const sg = $("#SetGrid");
@@ -12,10 +13,10 @@ function fillSets(sets){
                 `<div class="card p-2 setCard" value="${s[0]}">
                     <div class="row">
                         <div class="col-auto">
-                            <img class="img-fluid setimg" src="${s[2]}">
+                            <img class="img-fluid setimg" src="${s[1]}">
                         </div>
                         <div class="col-auto">
-                            <h5>${s[1]}</h5>
+                            <h5>${s[0]}</h5>
                         </div>
                     </div>
                 </div>`
@@ -25,6 +26,14 @@ function fillSets(sets){
         $(this).toggleClass("bg-primary"); 
      });
 }
+eel.expose(updateTeam);
+function updateTeam(teamstats){
+    let team =teamstats[0];
+    let teamImages = teamstats[1];
+    let count = teamstats[2];
+    $("#deck").val(team.join("\n"));
+    $("#nCards").text(count);  
+}
 $(document).ready(function(){
     eel.getSets()(fillSets);
     let cnt=0;
@@ -33,20 +42,18 @@ $(document).ready(function(){
         cnt++;
     });
     $(".option").click(function(){
+        $(".option").hide();
         eel.selectOption($(this).attr('option'))(setOptions);
     });
     
-    eel.expose(updateTeam);
-    function updateTeam(team){
-        $("#deck").val(team.join("\n"));     
-    }
-});
-$("#btnGoDraft").click(function(){
-    let sets = $(".bg-primary").map(function() {return $(this).attr('value');});
-    eel.setSets()(function(options){
-        setOptions(options);
-        $("#SetSelector").hide();
-        $("#CardSelector").show();
-    });
     
-})
+    $("#btnGoDraft").click(function(){
+        let sets = $(".bg-primary").map(function() {return $(this).attr('value');});
+        eel.setSets(sets.get())(function(options){
+            setOptions(options);
+            $("#SetSelector").hide();
+            $("#CardSelector").show();
+        });
+        
+    })
+});
